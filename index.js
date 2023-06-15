635cda337c0bb27e34957202d3cebf10938dab94
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-06-16 00:40:13 +0300

    completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = [
+"Milo",
+"Otis",
+"Garfield"
+];
+ 
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop('Garfield')
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift("Milo")
+}
+function appendCat(name){
+  return cats.concat('Broom') 
+}
+function prependCat(name){
+    return ['Arnold'].concat(cats);
+}
+function removeLastCat(name){
+    return cats.slice('0','-1')
+}
+function  removeFirstCat(name){
+    return cats.slice('1')
+}
